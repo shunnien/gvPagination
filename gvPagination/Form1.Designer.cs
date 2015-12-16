52f31bfaf50085d52a2dcb50b138a462910d377c
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2015-12-16 16:34:58 +0800

    介面設計 - 頁面切換按鍵新增 並設定按鍵 Name

--- gvPagination/Form1.Designer.cs
+++ gvPagination/Form1.Designer.cs
@@ -24,7 +24,10 @@
         /// </summary>
         private void InitializeComponent() {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnFirstPage = new System.Windows.Forms.Button();
+            this.btnPreviousPage = new System.Windows.Forms.Button();
+            this.btnNextPage = new System.Windows.Forms.Button();
+            this.btnLastPage = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,22 +40,55 @@
             this.dataGridView1.Size = new System.Drawing.Size(490, 321);
             this.dataGridView1.TabIndex = 0;
             // 
-            // button1
+            // btnFirstPage
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
-            this.button1.Location = new System.Drawing.Point(12, 339);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 40);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "第一頁";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnFirstPage.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnFirstPage.Location = new System.Drawing.Point(12, 339);
+            this.btnFirstPage.Name = "btnFirstPage";
+            this.btnFirstPage.Size = new System.Drawing.Size(90, 40);
+            this.btnFirstPage.TabIndex = 1;
+            this.btnFirstPage.Text = "第一頁";
+            this.btnFirstPage.UseVisualStyleBackColor = true;
+            // 
+            // btnPreviousPage
+            // 
+            this.btnPreviousPage.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPreviousPage.Location = new System.Drawing.Point(108, 339);
+            this.btnPreviousPage.Name = "btnPreviousPage";
+            this.btnPreviousPage.Size = new System.Drawing.Size(90, 40);
+            this.btnPreviousPage.TabIndex = 2;
+            this.btnPreviousPage.Text = "<<上一頁";
+            this.btnPreviousPage.UseVisualStyleBackColor = true;
+            // 
+            // btnNextPage
+            // 
+            this.btnNextPage.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnNextPage.Location = new System.Drawing.Point(316, 339);
+            this.btnNextPage.Name = "btnNextPage";
+            this.btnNextPage.Size = new System.Drawing.Size(90, 40);
+            this.btnNextPage.TabIndex = 3;
+            this.btnNextPage.Text = "下一頁>>";
+            this.btnNextPage.UseVisualStyleBackColor = true;
+            // 
+            // btnLastPage
+            // 
+            this.btnLastPage.Font = new System.Drawing.Font("Microsoft JhengHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnLastPage.Location = new System.Drawing.Point(412, 339);
+            this.btnLastPage.Name = "btnLastPage";
+            this.btnLastPage.Size = new System.Drawing.Size(90, 40);
+            this.btnLastPage.TabIndex = 4;
+            this.btnLastPage.Text = "最後一頁";
+            this.btnLastPage.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 476);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLastPage);
+            this.Controls.Add(this.btnNextPage);
+            this.Controls.Add(this.btnPreviousPage);
+            this.Controls.Add(this.btnFirstPage);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Pagination";
@@ -64,7 +100,10 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnFirstPage;
+        private System.Windows.Forms.Button btnPreviousPage;
+        private System.Windows.Forms.Button btnNextPage;
+        private System.Windows.Forms.Button btnLastPage;
     }
 }
 
