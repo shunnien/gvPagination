7195101f36fc30af40eb7301009e0b0b4872c5ac
Allen Yu (FD-TW) <allen_yu@trend.com.tw>
2015-12-16 17:11:14 +0800

    程式碼 - 加入 Configuration 參考並引用

--- gvPagination/Form1.cs
+++ gvPagination/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
@@ -10,6 +11,15 @@ using System.Windows.Forms;
 
 namespace gvPagination {
     public partial class Form1 : Form {
+
+        private string _connecString = ConfigurationManager.ConnectionStrings["DBConnection"].ConnectionString;
+        // 現在選取的分頁編號
+        private int _currentPageIndex = 1;
+        // 總共多少分頁
+        private int _totalPage = 0;
+        // 頁面大小
+        private int _pageSize = 20;
+
         public Form1() {
             InitializeComponent();
         }
